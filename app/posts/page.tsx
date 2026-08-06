2a503bcf85305265cd6b6aa4d03e9ca6161d8e70
BerlinBrick <olli.thal@gmx.de>
2026-08-06 13:08:25 +0200

    Posts Basis funktioniert

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import AppShell from "../components/AppShell";
+
+export default function PostsPage() {
+  return (
+    <AppShell>
+      <h1 className="text-4xl text-white">
+        Beiträge funktionieren
+      </h1>
+    </AppShell>
+  );
+}
